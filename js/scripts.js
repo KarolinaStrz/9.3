6a8d7bf1585f21d81a65efc51484cac95a83e1ce
Karolina Strzelecka <kstrzelecka0594@gmail.com>
2017-12-05 18:53:37 +0100

    Change .js file

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,10 @@ var text = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that firs
 
 var dinosaur = "triceratops";
 
-var dinosaurUpperCased = dinosaur.toUpperCase();
+ var dinosaurUpperCased = dinosaur.toUpperCase();
 
 var ChangeName = text.replace('Velociraptor', dinosaurUpperCased);
 
-
-var partOftext = text.slice(0,text.length/2);
+var partOftext = ChangeName.slice(0,ChangeName.length/2);
 
 console.log(partOftext);
